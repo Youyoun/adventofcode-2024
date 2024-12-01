ca7707942a7e3e798861116affc38d6e11fee92f
Corentin Godeau <corentin.godeau37@gmail.com>
2024-12-01 19:29:48 +0100

    corendos: day 01 (#12)

---
+++ day-01/part-2/corendos.zig
@@ -0,0 +1,70 @@
+const std = @import("std");
+
+var a: std.mem.Allocator = undefined;
+const stdout = std.io.getStdOut().writer(); //prepare stdout to write in
+
+pub fn lessThan(_: void, left: i64, right: i64) bool {
+    return left < right;
+}
+
+fn run(input: [:0]const u8) i64 {
+    var list1 = std.ArrayList(i64).initCapacity(a, 1000) catch unreachable;
+    var map = std.AutoHashMap(i64, i64).init(a);
+    var it = std.mem.tokenizeAny(u8, input, "\n ");
+    while (it.next()) |first_number_str| {
+        const second_number_str = it.next().?;
+        const first_number = std.fmt.parseInt(i64, first_number_str, 10) catch unreachable;
+        const second_number = std.fmt.parseInt(i64, second_number_str, 10) catch unreachable;
+
+        list1.append(first_number) catch unreachable;
+        const gop = map.getOrPut(second_number) catch unreachable;
+        if (!gop.found_existing) {
+            gop.value_ptr.* = 0;
+        }
+        gop.value_ptr.* += 1;
+    }
+    var acc: i64 = 0;
+
+    for (list1.items) |right| {
+        const similarity_score = right * (map.get(right) orelse 0);
+        acc += similarity_score;
+    }
+    return acc;
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator); // create memory allocator for strings
+
+    defer arena.deinit(); // clear memory
+    a = arena.allocator();
+
+    var arg_it = try std.process.argsWithAllocator(a);
+    _ = arg_it.skip(); // skip over exe name
+    const input: [:0]const u8 = arg_it.next().?;
+
+    const start: i128 = std.time.nanoTimestamp(); // start time
+    const answer = run(input); // compute answer
+    const end: i128 = std.time.nanoTimestamp();
+    const elapsed_nano: f64 = @floatFromInt(end - start);
+    const elapsed_milli = elapsed_nano / 1_000_000.0;
+    try stdout.print("_duration:{d}\n{}\n", .{ elapsed_milli, answer }); // emit actual lines parsed by AOC
+}
+
+test "example" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator); // create memory allocator for strings
+
+    defer arena.deinit(); // clear memory
+    a = arena.allocator();
+
+    const input =
+        \\3   4
+        \\4   3
+        \\2   5
+        \\1   3
+        \\3   9
+        \\3   3
+    ;
+
+    const result = run(input);
+    try std.testing.expectEqual(@as(i64, 31), result);
+}
