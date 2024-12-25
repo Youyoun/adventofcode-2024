9f966c27aa04274dfa737dea60b288f8e929c03e
Younes Belkouchi <younes.belkouchi@gleamer.ai>
2024-12-25 19:05:42 +0100

    youyoun: d9 p1

---
+++ day-09/part-2/youyoun.py
@@ -0,0 +1,24 @@
+from tool.runners.python import SubmissionPy
+
+
+class YouyounSubmission(SubmissionPy):
+    def run(self, s: str):
+        """
+        :param s: input in string format
+        :return: solution flag
+        """
+        # Your code goes here
+        pass
+
+
+def test_youyoun():
+    """
+    Run `python -m pytest ./day-09/part-2/youyoun.py` to test the submission.
+    """
+    assert (
+        YouyounSubmission().run(
+            """
+""".strip()
+        )
+        == None
+    )
