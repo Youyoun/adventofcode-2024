7f235cd4a388f72fa1097fd2c6587bc84391aafd
Younes Belkouchi <younes.belkouchi@gleamer.ai>
2024-12-25 13:59:30 +0100

    youyoun: d8 p2

--- day-08/part-2/youyoun.py
+++ day-08/part-2/youyoun.py
@@ -1,3 +1,7 @@
+import itertools
+import operator
+from collections import defaultdict
+
 from tool.runners.python import SubmissionPy
 
 
@@ -7,18 +11,65 @@ class YouyounSubmission(SubmissionPy):
         :param s: input in string format
         :return: solution flag
         """
-        # Your code goes here
-        pass
+        nodes = defaultdict(list)
+        lines = s.splitlines()
+        N = len(lines)
+        for x, l in enumerate(lines):
+            for y, c in enumerate(l):
+                if c != '.':
+                    nodes[c].append((x, y))
+        antinodes = set()
+        for freq in nodes:
+            for p1, p2 in itertools.combinations(nodes[freq], r=2):
+                antinodes.add(p1)
+                dx, dy = p2[0] - p1[0], p2[1] - p1[1]
+                for op in [operator.add, operator.sub]:
+                    an = op(p1[0], dx), op(p1[1], dy)
+                    while 0 <= an[0] < N and 0 <= an[1] < N:
+                        antinodes.add(an)
+                        an = op(an[0], dx), op(an[1], dy)
+        return len(antinodes)
 
 
 def test_youyoun():
     """
-    Run `python -m pytest ./day-08/part-2/youyoun.py` to test the submission.
+    Run `python -m pytest ./day-08/part-1/youyoun.py` to test the submission.
     """
     assert (
-        YouyounSubmission().run(
-            """
-""".strip()
-        )
-        == None
+            YouyounSubmission().run(
+                """............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............""".strip()
+            )
+            == 34
+    )
+
+
+def test_T():
+    """
+    Run `python -m pytest ./day-08/part-1/youyoun.py` to test the submission.
+    """
+    assert (
+            YouyounSubmission().run(
+                """T.........
+...T......
+.T........
+..........
+..........
+..........
+..........
+..........
+..........
+..........""".strip()
+            )
+            == 9
     )
