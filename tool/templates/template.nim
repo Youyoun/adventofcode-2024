3323cd840f3ba6ae2e23c23d5de800a23d9f1ee9
Ayc0 <ayc0.benj@gmail.com>
2025-08-08 00:26:45 +0200

    add day 1 nim

--- tool/templates/template.nim
+++ tool/templates/template.nim
@@ -1,15 +1,15 @@
 from times import cpuTime
 from os import paramStr
 
-proc run(s: string): string =
+proc run(s: var string): string =
     # Your code here
     return ""
 
 
 var input: string = paramStr(1)
 
-var t0 = cpuTime()
-var output = run(input)
+let t0 = cpuTime()
+let output = run(input)
 
 echo "_duration:", (cpuTime() - t0) * 1000
 echo output
